a152e5ce16758e99f2769e1bb5edec689c3c1292
mattshade <matt.shade@nbcuni.com>
2018-10-05 18:20:06 -0400

    updates some pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'm',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    // <h1>Hi people</h1>
+    <p>Welcome to your my website.</p>
+    // <p>Now go build something great.</p>
+    // <Link to="/page-2/">Go to page 2</Link>
+    <small>
+    &copy; matt shade.  
   </Layout>
 )
 
