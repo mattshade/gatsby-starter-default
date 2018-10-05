6d2db1d7106a0ac243a51f87bb98d0e4837cc7f8
mattshade <matt.shade@nbcuni.com>
2018-10-05 18:23:19 -0400

    small updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    // <h1>Hi people</h1>
-    <p>Welcome to your my website.</p>
-    // <p>Now go build something great.</p>
-    // <Link to="/page-2/">Go to page 2</Link>
-    <small>&copy; matt shade.</small>  
+    <p>Welcome to my website.</p>
+    <small>&copy; matt shade.</small>
   </Layout>
 )
 
