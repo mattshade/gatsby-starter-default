fcd582d3a790cb55df05dbadc6d2e7c4ecb6904c
mattshade <matt.shade@nbcuni.com>
2018-10-24 21:10:29 -0400

    adds redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <script>
+
   window.location = "https://nbcuniversal.webex.com/meet/pr206080731";
-  </script>
   
+
   <Layout>
   <h3>Welcome.</h3>
     <p>There is more to come... but for now this is all you get.</p>
