ecc2eace01a9203b47ab060ce9051585126f529b
mattshade <matt.shade@nbcuni.com>
2018-11-27 22:00:04 -0500

    adds p

--- src/pages/matts-iframe-a-testin.js
+++ src/pages/matts-iframe-a-testin.js
@@ -3,12 +3,14 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const SecondPage = () => (
+const IframePage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+  <p>hallo</p>
+    <script>
+      console.log("shady");
+      console.log(window.parent.AMP);
+    </script>
   </Layout>
 )
 
-export default SecondPage
+export default IframePage
